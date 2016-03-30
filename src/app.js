900e924574c27c2b21f2641aac25bf4aad9822a8
Eric Allen <decoy@internetalche.me>
2016-03-29 22:06:50 -0400

    added better dynamic buttons; added failure state; added reset functionality

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ require('./app.scss');
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import _ from 'lodash';
+
 import Solver from './utilities/solver';
 
 import {
@@ -25,6 +27,52 @@ const App = React.createClass({
     LEFT: 65, //A
     RIGHT: 68 //D
   },
+  buttonArray: [
+    {
+      key: 'playBtn',
+      action: 'startGame',
+      buttonText: {
+        default: 'Play',
+        playing: 'New Puzzle',
+        puzzleSolved: 'New Puzzle'
+      },
+      visibleStates: {
+        default: true,
+        playing: true,
+        puzzleSolved: true
+      }
+    },
+    {
+      key: 'resetBtn',
+      action: 'resetPuzzle',
+      buttonText: 'Reset Puzzle',
+      visibleStates: {
+        default: false,
+        playing: true,
+        puzzleSolved: false
+      }
+    },
+    {
+      key: 'solveBtn',
+      action: 'checkSolution',
+      buttonText: 'Check Solution',
+      visibleStates: {
+        default: false,
+        playing: true,
+        puzzleSolved: false
+      }
+    },
+    {
+      key: 'hintBtn',
+      action: 'getHint',
+      buttonText: 'Get Hint',
+      visibleStates: {
+        default: false,
+        playing: true,
+        puzzleSolved: false
+      }
+    },
+  ],
   startGame() {
     /*
     const cells = [
@@ -66,7 +114,7 @@ const App = React.createClass({
 
     const cells = [
       [9, 5, 3, 4, 2, 8, 7, 1, 6],
-      [2, 0, 0, 0, 0, 9, 8, 3, 5],
+      [2, 0, 1, 6, 7, 9, 8, 3, 5],
       [7, 8, 6, 3, 5, 1, 9, 4, 2],
       [8, 3, 5, 1, 9, 6, 4, 2, 7],
       [1, 9, 4, 7, 3, 2, 5, 6, 8],
@@ -83,12 +131,33 @@ const App = React.createClass({
       col: null
     };
 
-    this.setState({ cells, puzzle, currentFocus });
+    const playing = true;
+
+    this.setState({ cells, puzzle, currentFocus, playing });
   },
   checkSolution() {
-    const solution = new Solver(this.state.cells).checkSolution();
+    const puzzleChecked = true;
+    const puzzleSolved = new Solver(this.state.cells).checkSolution();
+    let playing = true;
 
-    console.log('Valid Solution:', solution);
+    if(puzzleSolved) {
+      playing = false;
+    }
+
+    this.setState({ puzzleSolved, playing, puzzleChecked });
+  },
+  puzzleCheckedState(puzzleChecked) {
+    this.setState({ puzzleChecked });
+  },
+  resetPuzzle() {
+    const cells = cloneArray(this.state.puzzle);
+
+    const puzzleChecked = false;
+
+    this.setState({ cells, puzzleChecked });
+  },
+  getHint() {
+    console.log('Give user hint');
   },
   setCurrentFocus(row = this.state.currentFocus.row, col = this.state.currentFocus.col, focus = false) {
     if(typeof row !== 'undefined' && typeof col !== 'undefined') {
@@ -147,13 +216,17 @@ const App = React.createClass({
   setValue(row, col, val) {
     const cells = this.state.cells;
 
-    cells[row][col] = val;
+    cells[row][col] = parseInt(val, 10);
 
     this.setState({ cells });
   },
   onKeyPress(e) {
     let { key } = e;
     let constants = this.keyBoardArrowConstants;
+
+    if(this.state.puzzleChecked) {
+      this.setState({ puzzleChecked: false });
+    }
 
     if(key === 'Unidentified') {
       key = e.which;
@@ -176,17 +249,65 @@ const App = React.createClass({
     return {
       cells: [],
       puzzle: [],
-      currentFocus: {}
+      currentFocus: {},
+      puzzleSolved: false,
+      playing: false,
+      puzzleChecked: false
     };
   },
   render() {
+    const buttons = this.buttonArray.map((button) => {
+      let { key, action, buttonText, visibleStates } = button;
+
+      const visibleStatesArray = _.keys(visibleStates);
+
+      let displayButton = false;
+
+      if(this.state.puzzleSolved && buttonText.hasOwnProperty('puzzleSolved')) {
+        buttonText = buttonText.puzzleSolved;
+      } else if(this.state.playing && buttonText.hasOwnProperty('playing')) {
+        buttonText = buttonText.playing;
+      } else if(buttonText.hasOwnProperty('default')) {
+        buttonText = buttonText.default;
+      }
+
+      if(visibleStatesArray.length) {
+        displayButton = visibleStatesArray.some((key) => {
+          if(key === 'default') {
+            return visibleStates[key];
+          }
+
+          if(this.state[key] && visibleStates[key]) {
+            return true;
+          }
+
+          return false;
+        });
+      }
+
+      if(displayButton) {
+        return <GameButton key={key} ref={key} action={this[action]} buttonText={buttonText} />
+      }
+    });
+
     return (
-      <div onKeyDown={this.onKeyPress}>
+      <div className="simpledoku-game"  onKeyDown={this.onKeyPress}>
         <h1>Simpledoku</h1>
-        <Game ref="game" rows={this.state.cells} puzzle={this.state.puzzle} buildRow={this.buildRow} moveFocus={this.moveFocus} setCurrentFocus={this.setCurrentFocus} keyBoardArrowConstants={this.keyBoardArrowConstants} keyBoardWASDConstants={this.keyBoardWASDConstants} currentFocus={this.state.currentFocus} setValue={this.setValue} />
-        <button onClick={this.startGame}>Play</button>
-        <button onClick={this.checkSolution}>Solve</button>
+        <Game ref="game" rows={this.state.cells} puzzle={this.state.puzzle} buildRow={this.buildRow} moveFocus={this.moveFocus} setCurrentFocus={this.setCurrentFocus} keyBoardArrowConstants={this.keyBoardArrowConstants} keyBoardWASDConstants={this.keyBoardWASDConstants} currentFocus={this.state.currentFocus} setValue={this.setValue} puzzleSolved={this.state.puzzleSolved} puzzleChecked={this.state.puzzleChecked} puzzleCheckedState={this.puzzleCheckedState}/>
+
+        { buttons }
+
       </div>
+    );
+  }
+});
+
+const GameButton = React.createClass({
+  render() {
+    const { key, action, buttonText } = this.props;
+
+    return (
+      <button className="simpledoku-button" key={key} onClick={action}>{buttonText}</button>
     );
   }
 });
@@ -195,13 +316,21 @@ const Game = React.createClass({
   render() {
     const rows = this.props.rows.map((cells, row) => {
       return (
-        <Row key={`row-${row}`} ref={`row-${row}`} cols={cells} row={row} puzzle={this.props.puzzle} moveFocus={this.props.moveFocus} setCurrentFocus={this.props.setCurrentFocus} keyBoardArrowConstants={this.props.keyBoardArrowConstants} keyBoardWASDConstants={this.props.keyBoardWASDConstants} currentFocus={this.props.currentFocus} setValue={this.props.setValue} />
+        <Row key={`row-${row}`} ref={`row-${row}`} cols={cells} row={row} puzzle={this.props.puzzle} moveFocus={this.props.moveFocus} setCurrentFocus={this.props.setCurrentFocus} keyBoardArrowConstants={this.props.keyBoardArrowConstants} keyBoardWASDConstants={this.props.keyBoardWASDConstants} currentFocus={this.props.currentFocus} setValue={this.props.setValue} puzzleCheckedState={this.props.puzzleCheckedState} />
       );
     });
 
+    let classes = ['simpledoku-grid'];
+
+    if(this.props.puzzleChecked && !this.props.puzzleSolved) {
+      classes.push('simpledoku-grid--failed');
+    } else if(this.props.puzzleSolved) {
+      classes.push('simpledoku-grid--solved');
+    }
+
     return (
-      <section className="simpledoku-game">
-        <table cellPadding="0" cellSpacing="0" className="simpledoku-grid">
+      <section className={classes.join(' ')}>
+        <table cellPadding="0" cellSpacing="0" className="simpledoku-grid-table">
           <tbody>
             {rows}
           </tbody>
@@ -214,11 +343,11 @@ const Game = React.createClass({
 const Row = React.createClass({
   render() {
     const cells = this.props.cols.map((val, col) =>{
-      return <Cell key={`row-${this.props.row}-col-${col}`} ref={`row-${this.props.row}-col-${col}`} val={val} puzzle={this.props.puzzle} row={this.props.row} col={col} moveFocus={this.props.moveFocus} setCurrentFocus={this.props.setCurrentFocus} keyBoardArrowConstants={this.props.keyBoardArrowConstants} keyBoardWASDConstants={this.props.keyBoardWASDConstants} currentFocus={this.props.currentFocus} setValue={this.props.setValue} />
+      return <Cell key={`row-${this.props.row}-col-${col}`} ref={`row-${this.props.row}-col-${col}`} val={val} puzzle={this.props.puzzle} row={this.props.row} col={col} moveFocus={this.props.moveFocus} setCurrentFocus={this.props.setCurrentFocus} keyBoardArrowConstants={this.props.keyBoardArrowConstants} keyBoardWASDConstants={this.props.keyBoardWASDConstants} currentFocus={this.props.currentFocus} setValue={this.props.setValue} puzzleCheckedState={this.props.puzzleCheckedState} />
     });
 
     return (
-      <tr>
+      <tr className="simpledoku-row">
         { cells }
       </tr>
     );
@@ -253,6 +382,8 @@ const Cell = React.createClass({
   onFocus(e) {
     const { row, col } = this.props;
 
+    this.props.puzzleCheckedState(false);
+
     this.props.setCurrentFocus(row, col);
   },
   render() {
@@ -263,7 +394,7 @@ const Cell = React.createClass({
         <input
           type="number"
           ref={`row-${this.props.row}-col-${this.props.col}-input`}
-          defaultValue={(this.props.val === 0) ? null : this.props.val}
+          value={(this.props.val === 0) ? null : this.props.val}
           readOnly={(parseInt(this.props.puzzle[this.props.row][this.props.col], 10) === 0) ? false : true}
           min="1"
           max="9"
